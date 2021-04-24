55d3e5a2816490b2a9112e9057ef57ecaf5f9a9c
mfsaglam <mfsaglam1@gmail.com>
2021-04-24 15:12:44 +0300

    now we have a DataManager

--- Racoon/Model/ItemManager.swift
+++ Racoon/Model/ItemManager.swift
@@ -8,5 +8,15 @@
 import Foundation
 
 struct ItemManager {
+    var dataArray: [Item] = [
+        Item(name: "Bacon Kg", packageQuantity: 100, unit: .kg),
+        Item(name: "Fish Can 250g", packageQuantity: 50, unit: .piece),
+        Item(name: "Patty pcs", packageQuantity: 100, unit: .piece),
+        Item(name: "Not Counted Item", packageQuantity: 0, unit: .kg),
+        Item(name: "Item with Inventory", packageQuantity: 9, unit: .piece, inventory: [Item.Inventory(amount: 100, type: .package), Item.Inventory(amount: 18, type: .piece)])
+    ]
     
+    func getData() -> [Item] {
+        return dataArray
+    }
 }
